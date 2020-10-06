6598a8d0abf55ea1c4ac4757c5df6cc1a9868dfd
Ilan Grapel <igrapel@users.noreply.github.com>
2020-10-06 08:00:07 -0400

    Update TaxReturn.java

--- AP_CSA/TaxReturn.java
+++ AP_CSA/TaxReturn.java
@@ -1,72 +1,107 @@
 
-package Topic_3;
+package Mod3;
 
-public class TaxReturn 
+import javax.swing.*;
+
+public class TaxReturn
 {
-double income;
-String status;
-//tax rates
+    private double income;
+    private String status;
+    private int ssn;
 
-private static final double RATE1 = 0.15;
-private static final double RATE2 = 0.20;
-private static final double RATE3 = 0.28;
-private static final double RATE4 = 0.37;
+    private static final double RATE_1 = 0.15;
+    private static final double RATE_2 = 0.20;
+    private static final double RATE_3 = 0.28;
+    private static final double RATE_4 = 0.37;
 
-private static final double MARRIED_BRACKET1 = 31000;
-private static final double MARRIED_BRACKET2 = 51400;
-private static final double MARRIED_BRACKET3 = 85670;
+    private static final double SINGLE_BRACKET_1 = 21000;
+    private static final double SINGLE_BRACKET_2 = 41400;
+    private static final double SINGLE_BRACKET_3 = 75670;
+    private static final double SINGLE_BRACKET_4 = 91340;
 
+    private static final double MARRIED_BRACKET_1 = 31000;
+    private static final double MARRIED_BRACKET_2 = 51400;
+    private static final double MARRIED_BRACKET_3 = 85670;
+    private static final double MARRIED_BRACKET_4 = 101340;
 
-private static final double SINGLE_BRACKET1 = 21000;
-private static final double SINGLE_BRACKET2 = 41400;
-private static final double SINGLE_BRACKET3 = 75670;
+    /*
+    * Create taxReturn for client object
+    * @param income taxpayer's income as double
+    * @param status taxpayer's marital status as String
+    */
 
-
-/**
- * create a taxreturn for client object
- * @param anIncome taxpayer's income as a double
- * @param astatus either single or married
- */
-
-public TaxReturn(double anIncome, String married)
-{
-    income = anIncome;
-    status = married;
-}
+    public TaxReturn(double income, String status, int ssn)
+    {
+        this.income = income;
+        this.status = status;
+        this.ssn = ssn;
+    }
     
-public double getTax()
-{
-   double tax = 0; 
-   if(status.equalsIgnoreCase("Married"))
-   {
-       if(income <= MARRIED_BRACKET1)
-       {
-           tax = income*RATE1;
-       }
-       
-       else if (income <= MARRIED_BRACKET2)
-       {
-           tax = RATE1*MARRIED_BRACKET1 + RATE2*(income - MARRIED_BRACKET1);
-       }
-       
-       else if (income <= MARRIED_BRACKET3)
-       {
-           tax = RATE1*MARRIED_BRACKET1 + RATE2*(MARRIED_BRACKET2 - MARRIED_BRACKET1) + RATE3*(income - MARRIED_BRACKET3);
-       }
-       
-       else
-       {
-           tax = RATE1*MARRIED_BRACKET1 + RATE2*(MARRIED_BRACKET2 - MARRIED_BRACKET1) + RATE3*(MARRIED_BRACKET3 - MARRIED_BRACKET2) + RATE4*(income - MARRIED_BRACKET3);
-       }
-       
-   }
-   else
-   {
-       //fill in
-   }
+    public int getSSN()
+    {
+        return ssn;
+    }
+
+    public double getTax()
+    {
+        double tax = 0;
+
+        if (status.equalsIgnoreCase("married") || status.equalsIgnoreCase("m") )
+        {
+            if (income <= MARRIED_BRACKET_1)
+            {
+                tax = income * RATE_1;
+            }
+            else if (income <= MARRIED_BRACKET_2)
+            {
+                tax = RATE_1 * MARRIED_BRACKET_1 + RATE_2 * (income - MARRIED_BRACKET_1);
+            }
+            else if (income <= MARRIED_BRACKET_3)
+            {
+                tax = RATE_1 * MARRIED_BRACKET_1 + RATE_2 * (MARRIED_BRACKET_2 - MARRIED_BRACKET_1) +
+                        RATE_3 * (income - MARRIED_BRACKET_2);
+            }
+            else
+            {
+                tax = RATE_1 * MARRIED_BRACKET_1 + RATE_2 * (MARRIED_BRACKET_2 - MARRIED_BRACKET_1) +
+                        RATE_3 * (MARRIED_BRACKET_3 - MARRIED_BRACKET_2) +
+                        RATE_4 * (income - MARRIED_BRACKET_4);
+            }
+        }
+        else if(status.equalsIgnoreCase("single") || status.equalsIgnoreCase("s")  )
+        {
+            if (income <= SINGLE_BRACKET_1)
+            {
+                tax = income * RATE_1;
+            }
+            else if (income <= SINGLE_BRACKET_2)
+            {
+                tax = RATE_1 * SINGLE_BRACKET_1 + RATE_2 * (income - SINGLE_BRACKET_1);
+            }
+            else if (income <= SINGLE_BRACKET_3)
+            {
+                tax = RATE_1 * SINGLE_BRACKET_1 + RATE_2 * (SINGLE_BRACKET_2 - SINGLE_BRACKET_1) +
+                        RATE_3 * (income - SINGLE_BRACKET_2);
+            }
+            else
+            {
+                tax = RATE_1 * SINGLE_BRACKET_1 + RATE_2 * (SINGLE_BRACKET_2 - SINGLE_BRACKET_1) +
+                        RATE_3 * (SINGLE_BRACKET_3 - SINGLE_BRACKET_2) +
+                        RATE_4 * (income - SINGLE_BRACKET_4);
+            }
+        }
+        
+        else
+        {
+            System.out.println("Inaccurate Status");
+        }
+
+        return tax;
+    }
     
-    return tax;
-}
-
-
+    //@Override
+    public boolean equals(TaxReturn o)
+    {
+        return o.getSSN()==this.getSSN();
+    }
 }
