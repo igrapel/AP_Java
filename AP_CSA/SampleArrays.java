cdee68dc8038673c6001d5d3ac29b2ad702a3603
Ilan Grapel <igrapel@users.noreply.github.com>
2020-11-18 12:28:11 -0500

    Update SampleArrays.java

--- AP_CSA/SampleArrays.java
+++ AP_CSA/SampleArrays.java
@@ -1,105 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Mod6;
 
-/**
- *
- * @author ilan
- */
-public class arrays {
+package Topic_6;
+
+public class array_exercises 
+{
+    public static void printArray(int[] a)
+    {
+       for(int element : a)
+       {
+           System.out.println(element);
+       }
+    }
     
-     public static void printArray(double[] arr)
-     {
-         for(double element : arr)
-         {
-             System.out.println(element);
-         }
-     }
-    
+    public static void printArray(String[] a)
+    {
+       for(String element : a)
+       {
+           System.out.print(element);
+       }
+    }
+   
     
     public static void main(String args[])
     {
-      
-        
-    System.out.println("I. 1000 numbers: ");
-    double[] arr1 = new double[2000];
-    for(int i = 0; i < arr1.length ; i++)
-    {
-        arr1[i] = (double)(i)/2;
-    }
-    //for all loop
-    
-        for(double element : arr1)
+       // 1. Create an array of multiples from 5 to 10k
+        int[] arr_fives = new int[2001];
+        for(int i = 0; i < arr_fives.length; i++)
         {
-            System.out.println(element);
+            arr_fives[i] = i*5;
         }
-    System.out.println("II. Odd numbers changed ");
-        int[] arr2 = new int[50];
-        for(int i = 0; i < arr2.length; i++)
+        
+        // 2. Array up to 100, every fifth elemtn is 777
+        int[] arr_lucky7 = new int[100];
+        for(int i = 0; i < arr_lucky7.length; i++)
         {
-            if(i%2 == 1)
+            if(i%5 == 0)
             {
-                arr2[i] =   (int)(2.5 *i - 12.5);
-            }
-            for(int oddIndex : arr2)
-            {
-                System.out.println(oddIndex);
+                arr_lucky7[i]= 777;
             }
             
-        }
-        //10 Random numbers 
-        System.out.println("III. Random Numbers: ");
-        double[] arr3 = new double[10];
-        for(int i = 0; i < arr3.length; i++)
-        {
-            arr3[i] = Math.random();
-        }
-        for(double rand: arr3)
-        {
-            System.out.println(rand);
-        }
-        
-        System.out.println("IV. Simulate SAT distribution: ");
-        // we want a mean of 175 and a SD of 250
-        int[] arr4 = new int[100000];
-        for(int i = 0; i<arr4.length; i++)
-        {
-            arr4[i] = (int)(Math.random()*1000) -325;
-        }
-        
-        for(int SAT : arr4)
-        {
-            System.out.println(SAT);
-        }
-        
-        int max = arr4[0];
-        
-        for(int m : arr4)
-        {
-            if(m > max)
+            else
             {
-                max = m;
+                arr_lucky7[i] = i;
             }
         }
         
-        System.out.println("V. Find sum of SATs: ");
-        double sum = 0;
-         for(int n : arr4)
+        // 6. Create array that holds 100 random dice rolls
+        int[] die100 = new int[100];
+        for(int i = 0; i < die100.length; i++)
         {
-            sum = sum + n;
+            die100[i] = (int)(Math.random()*6 + 1);
+            
         }
-         double mean = sum/arr4.length;
-        System.out.println("Joaqy's max verbal score is: " + max);
-        System.out.println("Joaqy's mean verbal score is: " + mean);
         
         
+        // 7. Simulate SAT scores for 10 students. Mean 500 and SD 80
+        int[] satSim = new int[10];
+        for(int i = 0; i < satSim.length; i++)
+        {
+            satSim[i] = (int)(Math.random()*50 + 500);
+        }
+        
+         //8. Use String arrays to create a checker board
+         String[] rowEven = new String[8];
+         String[] rowOdd = new String[8];
+         
+         for(int i = 0; i < rowEven.length; i++)
+         {
+             if(i%2 == 0)
+             {
+                rowEven[i] = "X";
+                rowOdd[i] = "0";
+             }
+             else
+             {
+                 rowEven[i] = "O";
+                rowOdd[i] = "X";
+             }
+             
+         }
+        
+         for(int i = 0; i < 7; i++)
+         {
+             if(i%2 == 0)
+             {
+                 printArray(rowEven);
+             }
+             else
+             {
+                 printArray(rowOdd);
+             }
+             System.out.println();
+         }
         
         
-        
+       
         
     }
+    
     
 }
