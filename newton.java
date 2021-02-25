bd776895bcfdd2add2ca464e19c24f4c73a07831
Ilan Grapel <igrapel@users.noreply.github.com>
2021-02-25 15:18:00 -0500

    Update newton.java

--- newton.java
+++ newton.java
@@ -1,74 +1,70 @@
-public class newton{
- 
-    static final double EPSILON = 0.001; 
-    int a;
-    int b;
-    int c;
-    int d;
-    int e;
-    int count = 0;
+public class poly 
+{
+    static final double EPSILON = .00001;
+    //instance variables are my coefficients
+    private int fourth;
+    private int third;
+    private int second;
+    private int first;
+    private int constant;
     
-    public newton(int a, int b, int c, int d, int e)
-    {
-     this.a = a;
-     this.b = b;
-     this.c = c;
-     this.d = d;
-     this.e = e;
-    }
+public poly(int fourth, int third, int second, int first, int c)
+{
+    this.fourth = fourth;
+    this.third = third;
+    this.second = second;
+    this.first = first;
+    constant = c;
+}
 
-    public newton(int b, int c, int d, int e)
-    {
-     this.a = 0;
-     this.b = b;
-     this.c = c;
-     this.d = d;
-     this.e = e;
-    }
-    // An example function 
-     // The function is x^3 - x^2 + 2 
-    public double func(double x) 
-    { 
-        return a*Math.pow(x,4) + b*Math.pow(x, 3) + c * Math.pow(x,2) + d*Math.pow(x,1) + e*Math.pow(x,0); 
-    } 
-      
-    // Derivative of the above function  
-    
-    public double derivFunc(double x) 
-    { 
-        return 4*a*Math.pow(x,3) + 3 * b * Math.pow(x,2) + 2 * c * Math.pow(x,1) + d; 
-    } 
-    
-    // Function to find the root 
-    public void newtonRaphson(double x) 
-    {    
-        double h = func(x) / derivFunc(x); 
-        while (Math.abs(h) >= EPSILON) 
-        { 
-            h = func(x) / derivFunc(x); 
-      
-            // x(i+1) = x(i) - f(x) / f'(x)  
-            x = x - h; 
-            count++;
+public poly(int third, int second, int first, int c)
+{
+    this.fourth = 0;
+    this.third = third;
+    this.second = second;
+    this.first = first;
+    constant = c;
+}
 
-            if(count > 100)
-            {
-                System.out.println("No real solutions.");
-                break;
-            }
-        } 
-        if(count<=100)
-        {
-        System.out.print("after iterations: " + count + " The value of the" + " root is : " 
-            + Math.round(x * 100.0) / 100.0); 
-        }  
-    } 
-      
-    public static void main(String[] args)
-    {
-        newton ex1 = new newton(1, -2, -3, 1, 1);
        
-        double x0 = -40;  
-        ex1.newtonRaphson(x0);
+public double funcValue(double x)
+{
+    return fourth * Math.pow(x, 4) + third * Math.pow(x,3) + second * Math.pow(x,2) + first * Math.pow(x, 1) + constant;
+}
+   
+public double derivFunction(double x)
+{
+    return 4*fourth * Math.pow(x,3)+ 3*third*Math.pow(x, 2) + 2*second*Math.pow(x, 1) + first;
+}
+
+//give random input to get back the zero
+public void newtonRaphson(double x)
+{
+    int counter = 0;
+    double ratio = funcValue(x) / derivFunction(x);
+    while(Math.abs(ratio) >= EPSILON)
+    {
+        counter++;
+        ratio = funcValue(x) / derivFunction(x);
+        x = x - ratio;
+        
+        
+        if(counter > 100)
+        {
+            System.out.println("There are no real roots");
+            return;
+        }
     }
+    
+    System.out.println("The value of the root is: " + Math.round(x*100.0) / 100.0);
+    System.out.println("Iterations were: " + counter);
+}
+
+public static void main(String[] args)
+{
+    poly ex1 = new poly(0, 0, 1, 0, 0);
+    System.out.println(ex1.funcValue(1));
+    System.out.println(ex1.derivFunction(1));
+    ex1.newtonRaphson(1);
+}
 }
