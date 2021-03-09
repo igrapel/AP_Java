16cda8f6347f31f0acada02b4fcdb9bd0316c850
Ilan Grapel <igrapel@users.noreply.github.com>
2021-03-08 21:46:30 -0500

    Update RSA.java

--- RSA.java
+++ RSA.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Topic_10;
-
 import java.util.ArrayList;
+import java.math.BigInteger;
 
 public class RSA 
 {
@@ -14,6 +8,7 @@ public class RSA
  int public_key_modulus;
  int phi;
  ArrayList<Integer> keys;
+ ArrayList<Integer> private_keys;
  
  public RSA(int prime1, int prime2)
  {
@@ -25,7 +20,7 @@ public class RSA
     
  public static int gcf(int a, int b)
     {
-       int greatestFactor = 1;
+        int greatestFactor = 1;
        
        if(a%b == 0)
        {
@@ -34,8 +29,8 @@ public class RSA
        }
        else
        {
-           int remainder = a % b;
-           greatestFactor = gcf(b, remainder);
+        int remainder = a % b;
+        greatestFactor = gcf(b, remainder);
        }
        
        return greatestFactor;
@@ -56,16 +51,49 @@ public class RSA
      return keys;
  }
  
- public ArrayList<Integer> private_key_generator(int key)
+ 
+//Basic Equation:
+//Private Key * Public Key Exponent mod phi = 1 $
+ public ArrayList<Integer> private_key_generator(int public_key_exp, int distance)
  {
-     
-     
+     private_keys = new ArrayList<Integer>();
+     for(int i = 1; i < distance; i++)
+     {
+            if(i*public_key_exp%this.phi == 1)
+            {
+                private_keys.add(i);
+            }
+     }
+     return private_keys;
+ }
+
+ public BigInteger encrypt(int n, int publicK)
+ {
+    BigInteger mod = BigInteger.valueOf(public_key_modulus); 
+    BigInteger biginteger = BigInteger.valueOf(n);
+    BigInteger encrypted = biginteger.pow(publicK);
+    encrypted = encrypted.mod(mod);
+    return encrypted;
+ }
+
+ public BigInteger decrypt(int encrypted, int privateKey)
+ {
+    BigInteger biginteger = BigInteger.valueOf(encrypted);
+    BigInteger answer = biginteger.pow(privateKey);
+    BigInteger modulus = BigInteger.valueOf(public_key_modulus);
+    answer = answer.mod(modulus);
+    return answer;  
  }
  
-    public static void main(String[] args) {
-        RSA transmission = new RSA(5, 11);
-        transmission.possible_public_keys();
-        System.out.println(transmission.keys);
-    }
+public static void main(String[] args) 
+{
+    int theNum = 2;
+    RSA transmission = new RSA(5, 11);
+    transmission.possible_public_keys();
+    System.out.println(transmission.keys);
+    System.out.println(transmission.private_key_generator(7, 200));
+    System.out.println("Plain: " + theNum + "\nEncrypted: " + transmission.encrypt(theNum, 7));
+    System.out.println("Unencrypted: " + transmission.decrypt(18, 23));
+}
     
 }
