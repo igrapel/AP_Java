2b29449424911f06250cb5af6954d18294362fed
Ilan Grapel <igrapel@users.noreply.github.com>
2021-05-12 12:43:01 -0400

    Create WordPair.java

---
+++ AP_CSA/FRQ/2018/WordPair.java
@@ -0,0 +1,21 @@
+public class WordPair
+{
+    String first, second;
+
+    public WordPair(string first, String second)
+    {
+       this.first = first;
+       this.second = second;
+    }
+
+    public String getFirst()
+    {
+        return first;
+    }
+
+    public String getSecond()
+    {
+        return second;
+    }
+
+}
