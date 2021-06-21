ac920c8b4d7afb02187a4184e8ce103bec0e95c4
Ilan Grapel <igrapel@users.noreply.github.com>
2021-06-20 21:45:06 -0400

    Update BruteCollinear.java

--- algorithms/week3/BruteCollinear.java
+++ algorithms/week3/BruteCollinear.java
@@ -1,31 +1,24 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.ArrayList;
 
 public class BruteCollinearPoints {
-    private ArrayList<LineSegment> lines = new ArrayList<LineSegment>();
+    private final ArrayList<LineSegment> lines = new ArrayList<LineSegment>();
 
     public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
     {
-        if(points == null)
-        {
+        if (points == null) {
             throw new IllegalArgumentException("Null Values.");
         }
-        for(Point p : points)
-        {
-            if(p == null)
-            {
+        for (Point p : points) {
+            if (p == null) {
                 throw new IllegalArgumentException("Null Values.");
             }
         }
-
+        
         Arrays.sort(points);
-        for(int a = 0; a <= points.length - 4; a++)
-        {
-            for(int b = a + 1; b <= points.length - 3; b++)
-            {
-                for(int c = b + 1; c <= points.length - 2; c++)
-                {
+        for (int a = 0; a <= points.length - 4; a++) {
+            for (int b = a + 1; b <= points.length - 3; b++) {
+                for (int c = b + 1; c <= points.length - 2; c++) {
                     /**
                     System.out.println(("Iteration: "));
                     System.out.println("First Point:  " +points[a]);
@@ -35,9 +28,9 @@ public class BruteCollinearPoints {
                     System.out.println("Second Slope:" + points[a].slopeTo(points[b]));
                     System.out.println("Third Slope:" + points[a].slopeTo(points[c]));
                     //System.out.println("Fourth Slope:" + points[a].slopeTo(points[d]));**/
-                    if(points[a].slopeTo(points[b]) == points[a].slopeTo(points[c]))
+                    if (points[a].slopeTo(points[b]) == points[a].slopeTo(points[c]))
                     {
-                        for(int d = c + 1; d <= points.length - 1; d++)
+                        for (int d = c + 1; d <= points.length - 1; d++)
                         {
                             /**
                             System.out.println(("Iteration: "));
@@ -48,15 +41,15 @@ public class BruteCollinearPoints {
                             System.out.println("Second Slope:" + points[a].slopeTo(points[b]));
                             System.out.println("Third Slope:" + points[a].slopeTo(points[c]));
                             System.out.println("Fourth Slope:" + points[a].slopeTo(points[d]));**/
-                            if(points[a].slopeTo(points[c]) == points[a].slopeTo(points[d]))
+                            if (points[a].slopeTo(points[c]) == points[a].slopeTo(points[d]))
                             {
-                                System.out.println("Last Loop: ");
+                                /** System.out.println("Last Loop: ");
                                 System.out.println("First Point:  " +points[a]);
                                 System.out.println("Fourth Point: " + points[d]);
                                 System.out.println("Third Slope:" + points[a].slopeTo(points[c]));
-                                System.out.println("Fourth Slope:" + points[a].slopeTo(points[d]));
-                                LineSegment l = new LineSegment(points[a], points[d]);
-                                lines.add(l);
+                                System.out.println("Fourth Slope:" + points[a].slopeTo(points[d]));**/
+                                LineSegment line = new LineSegment(points[a], points[d]);
+                                lines.add(line);
                             }
                         }
                     }
@@ -80,7 +73,7 @@ public class BruteCollinearPoints {
      */
     public static void main(String[] args) {
         /* YOUR CODE HERE */
-        Point[] testPoints = new Point[10];
+        /** Point[] testPoints = new Point[10];
 
         testPoints[1] = new Point(20000,21000);
         testPoints[2] = new Point(3, 91);
@@ -103,5 +96,6 @@ public class BruteCollinearPoints {
             System.out.println(ls);
         }
 
-    }
+    }**/
+}
 }
