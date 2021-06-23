4c9f8d4ba3fa386b28bb27d9aa214bf799793ef1
Ilan Grapel <igrapel@users.noreply.github.com>
2021-06-22 20:33:15 -0400

    Update FastCollinear.java

--- algorithms/week3/FastCollinear.java
+++ algorithms/week3/FastCollinear.java
@@ -18,33 +18,61 @@ public class FastCollinearPoints {
             }
         }
 
+        Point[] orderedPoints = new Point[points.length];
+        for (int i = 0; i < points.length; i++)
+        {
+            orderedPoints[i] = points[i];
+        }
+        Arrays.sort(orderedPoints);
+
+        Point[] orderedPointsSlope = new Point[points.length];
+        for (int i = 0; i < points.length; i++)
+        {
+            orderedPointsSlope[i] = points[i];
+        }
+        Arrays.sort(orderedPointsSlope);
+
         for (int i = 0; i <= points.length - 4; i++)
         {
-            // System.out.println("Loop" + i);
-            // System.out.println("Origin Point: " + points[i]);
-
-            int count = 0;
-            int indexPoint = 1;
-            Arrays.sort(points, points[i].slopeOrder());
-
-            /** for(Point p : points)
+            /** System.out.println("Loop: " + i);
+            System.out.println("Origin Point: " + orderedPoints[i]);
+            for(Point p : orderedPoints)
             {
                 System.out.println(p);
-            } */
-
+            }*/
+            int count = 0;
+            int indexPoint = 1;
+            Arrays.sort(orderedPointsSlope);
+            Arrays.sort(orderedPointsSlope, orderedPoints[i].slopeOrder());
+            /** System.out.println("Slope Sort");
+            for(Point p : orderedPointsSlope)
+            {
+                System.out.println(p);
+            }*/
+            Point min = orderedPointsSlope[0];
             while (indexPoint <= (points.length - 2))
             {
-                // System.out.println("Slope: " + index_point + ": " + points[0].slopeTo(points[index_point]));
-                if (points[0].slopeTo(points[indexPoint]) == points[0].slopeTo(points[indexPoint +1]))
+                // System.out.println("Slope: " + indexPoint + ": " + orderedPointsSlope[0].slopeTo(orderedPointsSlope[indexPoint]));
+
+                if (orderedPointsSlope[0].slopeTo(orderedPointsSlope[indexPoint]) == orderedPointsSlope[0].slopeTo(orderedPointsSlope[indexPoint +1]))
                 {
+                    if (orderedPointsSlope[indexPoint].compareTo(min) < 1)
+                    {
+                        min = orderedPointsSlope[indexPoint];
+                    }
                     // System.out.println("Here");
                     count++;
                 }
-                if (points[0].slopeTo(points[indexPoint]) != points[0].slopeTo(points[indexPoint +1]) && (count >= 2))
+                if (orderedPointsSlope[0].slopeTo(orderedPointsSlope[indexPoint]) != orderedPointsSlope[0].slopeTo(orderedPointsSlope[indexPoint +1])
+                        && (count >= 2))
                 {
-                    LineSegment line = new LineSegment(points[0], points[indexPoint]);
-                    lines.add(line);
-                    count = 0;
+                    // System.out.println("Min: " + min);
+                        if (min.compareTo(orderedPointsSlope[indexPoint]) == 0 || min.compareTo(orderedPointsSlope[0]) == 0)
+                        {
+                            LineSegment line = new LineSegment(orderedPointsSlope[0], orderedPointsSlope[indexPoint]);
+                            lines.add(line);
+                            count = 0;
+                        }
                 }
                 indexPoint++;
             }
@@ -64,18 +92,16 @@ public class FastCollinearPoints {
 
     public static void main(String[] args)
     {
-        /** Point[] testPoints = new Point[10];
+         /** Point[] testPoints = new Point[8];
 
-        testPoints[0] = new Point(6000, 7000);
-        testPoints[1] = new Point(20000,21000);
-        testPoints[2] = new Point(3, 91);
-        testPoints[3] = new Point(-3, -11);
-        testPoints[4] = new Point(3000, 7000);
-        testPoints[5] = new Point(10000, 0);
-        testPoints[6] = new Point(0, 10000);
-        testPoints[7] = new Point(7000,3000);
-        testPoints[8] = new Point(3000, 4000);
-        testPoints[9] = new Point(14000, 15000);
+        testPoints[0] = new Point(10000, 0);
+        testPoints[1] = new Point(0,10000);
+        testPoints[2] = new Point(3000, 7000);
+        testPoints[3] = new Point(7000, 3000);
+        testPoints[4] = new Point(20000, 21000);
+        testPoints[5] = new Point(3000,4000);
+        testPoints[6] = new Point(14000, 15000);
+        testPoints[7] = new Point(6000, 7000);
 
 
         FastCollinearPoints test = new FastCollinearPoints(testPoints);
@@ -86,8 +112,7 @@ public class FastCollinearPoints {
         for (LineSegment ls : lines)
         {
             System.out.println(ls);
-        }
+        }*/
 
-    }**/
-}
+    }
 }
