e10a6cdf55a8064a181336822a32c85b6578f317
Ilan Grapel <igrapel@users.noreply.github.com>
2021-09-23 15:01:08 -0400

    Update TaxReturnTester.java

--- AP_CSA/Topic_3/TaxReturnTester.java
+++ AP_CSA/Topic_3/TaxReturnTester.java
@@ -1,104 +1,21 @@
-package Topic_3;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topic3;
 
-import javax.swing.*;
 
-public class TaxReturn
-{
-    private double income;
-    private String status;
-    private int ssn;
-   
-    private static final double RATE_1 = 0.15;
-    private static final double RATE_2 = 0.20;
-    private static final double RATE_3 = 0.28;
-    private static final double RATE_4 = 0.37;
+public class Topic3 {
 
-    private static final double SINGLE_BRACKET_1 = 21000;
-    private static final double SINGLE_BRACKET_2 = 41400;
-    private static final double SINGLE_BRACKET_3 = 75670;
-    private static final double SINGLE_BRACKET_4 = 91340;
-
-    private static final double MARRIED_BRACKET_1 = 31000;
-    private static final double MARRIED_BRACKET_2 = 51400;
-    private static final double MARRIED_BRACKET_3 = 85670;
-    private static final double MARRIED_BRACKET_4 = 101340;
-
-    /*
-    * Create taxReturn for client object
-    * @param income taxpayer's income as double
-    * @param status taxpayer's marital status as String
-    */
-
-    public TaxReturn(double income, String status, int ssn)
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
     {
-        this.income = income;
-        this.status = status;
-        this.ssn = ssn;
-    }
-   
-    public double getTax()
-    {
-        double tax = 0;
-
-        if (status.equalsIgnoreCase("married") || status.equalsIgnoreCase("m") )
-        {
-            if (income <= MARRIED_BRACKET_1)
-            {
-                tax = income * RATE_1;
-            }
-            else if (income <= MARRIED_BRACKET_2)
-            {
-                tax = RATE_1 * MARRIED_BRACKET_1 + RATE_2 * (income - MARRIED_BRACKET_1);
-            }
-            else if (income <= MARRIED_BRACKET_3)
-            {
-                tax = RATE_1 * MARRIED_BRACKET_1 + RATE_2 * (MARRIED_BRACKET_2 - MARRIED_BRACKET_1) +
-                        RATE_3 * (income - MARRIED_BRACKET_2);
-            }
-            else
-            {
-                tax = RATE_1 * MARRIED_BRACKET_1 + RATE_2 * (MARRIED_BRACKET_2 - MARRIED_BRACKET_1) +
-                        RATE_3 * (MARRIED_BRACKET_3 - MARRIED_BRACKET_2) +
-                        RATE_4 * (income - MARRIED_BRACKET_4);
-            }
-        }
-        else if((status.equalsIgnoreCase("single") || status.equalsIgnoreCase("s") ))
-        {
-            if (income <= SINGLE_BRACKET_1)
-            {
-                tax = income * RATE_1;
-            }
-            else if (income <= SINGLE_BRACKET_2)
-            {
-                tax = RATE_1 * SINGLE_BRACKET_1 + RATE_2 * (income - SINGLE_BRACKET_1);
-            }
-            else if (income <= SINGLE_BRACKET_3)
-            {
-                tax = RATE_1 * SINGLE_BRACKET_1 + RATE_2 * (SINGLE_BRACKET_2 - SINGLE_BRACKET_1) +
-                        RATE_3 * (income - SINGLE_BRACKET_2);
-            }
-            else
-            {
-                tax = RATE_1 * SINGLE_BRACKET_1 + RATE_2 * (SINGLE_BRACKET_2 - SINGLE_BRACKET_1) +
-                        RATE_3 * (SINGLE_BRACKET_3 - SINGLE_BRACKET_2) +
-                        RATE_4 * (income - SINGLE_BRACKET_4);
-            }
-        }
-        else{
-            System.out.println("It looks like you have a marital status input error");
-        }
-   
-        return tax;
+      TaxReturn kateB = new TaxReturn(33000, false, 19222, "loveCSA");
+      System.out.println("Kate's tax: " + kateB.getTax("1234"));
+        
     }
     
-    public int getSSN()
-    {
-        return ssn;
-    }
-    
-    //@override
-    public boolean equals(TaxReturn c)
-    {
-        return (this.getSSN()==c.getSSN()) && (this.income == c.income);
-    }
 }
