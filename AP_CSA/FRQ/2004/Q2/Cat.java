0c1ec8fc76f52190cec88561103a85c1edf1409d
Ilan Grapel <igrapel@users.noreply.github.com>
2022-02-08 19:49:03 -0500

    Create Cat.java

---
+++ AP_CSA/FRQ/2004/Q2/Cat.java
@@ -0,0 +1,14 @@
+package Y2004.Q2;
+
+public class Cat extends Pet
+{
+    public Cat(String name)
+    {
+        super(name);
+    }
+
+    public String speak()
+    {
+        return "meow";
+    }
+}
