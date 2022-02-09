ac5886356820838bd1e42f0f7a53b2f003eab295
Ilan Grapel <igrapel@users.noreply.github.com>
2022-02-08 19:48:36 -0500

    Create Dog.java

---
+++ AP_CSA/FRQ/2004/Q2/Dog.java
@@ -0,0 +1,14 @@
+package Y2004.Q2;
+
+public class Dog extends Pet
+{
+    public Dog(String name)
+    {
+        super(name);
+    }
+
+    public String speak()
+    {
+        return "dog-sound";
+    }
+}
